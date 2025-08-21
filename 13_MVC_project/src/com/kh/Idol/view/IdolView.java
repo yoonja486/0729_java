44ea676ed5675bdc83210f9ac796e3aa539d0315
yoonja486 <yoonja486@gmail.com>
2025-08-21 17:43:19 +0900

    Idol

--- 13_MVC_project/src/com/kh/Idol/view/IdolView.java
+++ 13_MVC_project/src/com/kh/Idol/view/IdolView.java
@@ -199,7 +199,7 @@ public class IdolView {
 			switch(menuNo) {
 			case 1 : post(); break;
 			case 2 : selectBoardList(); break;
-			case 3 : break;
+			case 3 : findByBoardNo(); break;
 			case 4 : return;
 			}
 		}
@@ -255,5 +255,27 @@ public class IdolView {
 		}
 	}
 	
+	private void findByBoardNo() {
+		
+		System.out.println("\n게시글 상세 보기 서비스 입니다.");
+		selectBoardList();
+		
+		System.out.println("상세 보기를 할 번호를 입력하세요 > ");
+		int boardNo = sc.nextInt();
+		sc.nextLine();
+		
+		Board board = ic.findByBoardNo(boardNo);
+		
+		if(board != null) {
+			System.out.println("===========================================");
+			System.out.println(boardNo + "번 게시글 상세보기");
+			System.out.println("\n제목 : " + board.getBoardTitle());
+			System.out.println("\n내용 : " + board.getBoardContent());
+			System.out.println("\n작성자 : " + board.getUserId() + "\t 작성일 : " + board.getCreateDate());
+			
+		} else {
+			System.out.println(boardNo + "번 게시글은 존재하지 않습니다.");
+		}
+	}
 	
 }
